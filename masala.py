b39e51a076a38bea29091a149ce09d7fcd8c12f7
EXA_GAMER_007 <salimovsarvar518@gmail.com>
2025-10-18 18:22:51 +0500

    Create masala.py

---
+++ masala.py
@@ -0,0 +1,19 @@
+# jins = input("Jins kiriting: ").lower()
+# yosh = int(input("Yoshingizni kiriting: "))
+# if jins == "erkak" or "ayol":
+#    n_y = yosh - 60
+#    if jins == "erkak" and  yosh >= 60 and jins == "erkak" and yosh <= 120:
+#       print("Bobo siz pensiya yoshidasiz")
+#       print(f"Siz {n_y} yildan beri nafaqadasiz!")
+#    else:
+#        print("Mijoz siz pensiya yoshida emassiz!")
+#        print(f"Siz Yaqinda pensiyaga chiqasiz!")
+#        n_y = yosh - 55
+#        if yosh == "ayol" and yosh >= 55 and jins == "ayol" and yosh <= 120:
+#            print("Buvi siz pensiya yoshidasiz!")
+#            print(f"Siz {n_y} yildan beri pensiyadasiz!")
+#        else:
+#             print("Mijoz siz pensiya yoshida emassiz!")
+#             print("Siz yaqinda pensiyaga chiqasiz!")
+#             if 0 < yosh >= 120:
+#                 print("Yosh xato kiritilgan!")
